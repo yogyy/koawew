fc618a2572b56c7ebedfc9d415f6da457f86079e
yogyy <yogimr261@gmail.com>
2024-04-20 11:03:55 +0700

    chore: move to models

--- src/libs/auth.js
+++ src/libs/auth.js
@@ -3,29 +3,6 @@ import * as jose from "jose";
 import { secret } from "./utils.js";
 
 /**
- * @typedef {Object} User
- * @property {string} id - The unique identifier of the user.
- * @property {string} email - The email address of the user.
- * @property {string} password - The hashed password of the user.
- * @property {string} username - The username of the user.
- * @property {string} created_at - The timestamp when the user was created.
- * @property {string} updated_at - The timestamp when the user was last updated.
- */
-
-/**
- * Finds a user by email.
- * @param {string} email - The email address of the user.
- * @returns {Promise<{ user: User, isExist: boolean }>} A promise that resolves to an object containing the user and a boolean indicating whether the user exists.
- */
-export async function findUser(email) {
-  const [user] = await db("koa_user")
-    .select("*")
-    .whereRaw("koa_user.email = ?", email);
-
-  return { user, isExist: !!user };
-}
-
-/**
  * @param {import("jose").JWTPayload & {sub: string, email: string}} payload
  */
 export async function generateToken(payload) {
